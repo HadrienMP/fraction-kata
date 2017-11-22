8f2731d5d9dde20b654f830ac836e5e788d6f3c5
Hadrien Mens-Pellen <hmens@financeactive.com>
2017-11-22 13:38:03 +0100

    Initial commit

---
+++ src/test/java/fr/hadrienmp/katas/fraction/FractionTest.java
@@ -0,0 +1,34 @@
+package fr.hadrienmp.katas.fraction;
+
+import org.junit.Test;
+
+public class FractionTest {
+    /*
+      * Behaviour slicing
+      * =================
+      *
+      * Addition
+      * --------
+      *
+      * --> not a fraction
+      * 0 + 0 = 0
+      * 1 + 0 = 1
+      * 0 + 1 = 1
+      * 1 + 2 = 3
+      *
+      * 1/3 + 1/3 = 2/3 --> same denominator
+      * 1/2 + 1/3 = 3/6 + 2/6 = 5/6  --> different denominators
+      *
+      * 3/1 + 0 = 3  --> simplification
+      * 9/3 + 0 = 3 --> simplification
+      *
+      * 1/4 + 2/8 = 1/4 + 1/4 = 2/4 = 1/2 --> different denominators with simplifcation
+      *
+      *
+      * Substraction
+      * Multiplication
+      * Division
+      *
+      *
+      */
+}
